5e4603d7742309593d880d671c54733f8a332fa7
Mario Casola <m.casola@uqu.do>
2023-12-14 18:16:54 +0700

    Updated SDK to version 3.0.0.PRE2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UqudoSDK-Preview",
-            url: "https://rm.dev.uqudo.io/repository/uqudo-public/io/uqudo/sdk/iOS-Uqudo-Preview/3.0.0.PRE1/iOS-Uqudo-Preview-3.0.0.PRE1.zip",
-            checksum: "ca15493c2e67fffe4a8488eae5685fd74835a1ed32059565fa41560f15bf3690"
+            url: "https://rm.dev.uqudo.io/repository/uqudo-public/io/uqudo/sdk/iOS-Uqudo-Preview/3.0.0.PRE2/iOS-Uqudo-Preview-3.0.0.PRE2.zip",
+            checksum: "14a56f4c88372644da2855d31c4d7e8554ce0d0a3f51b352ec8d2f45a740426c"
         )
     ]
 )
