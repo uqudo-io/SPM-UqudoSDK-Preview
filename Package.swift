a3441088a96b143c7967f7254512d35582e57f41
Mario Casola <m.casola@uqu.do>
2024-02-02 21:51:25 +0700

    Released 3.0.0.PRE4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UqudoSDK-Preview",
-            url: "https://rm.dev.uqudo.io/repository/uqudo-public/io/uqudo/sdk/iOS-Uqudo-Preview/3.0.0.PRE3/iOS-Uqudo-Preview-3.0.0.PRE3.zip",
-            checksum: "c3d6beff8b2a08fa968ffbfefd68d5dfa8e882ea1ef593744e941a4de5da8d94"
+            url: "https://rm.dev.uqudo.io/repository/uqudo-public/io/uqudo/sdk/iOS-Uqudo-Preview/3.0.0.PRE4/iOS-Uqudo-Preview-3.0.0.PRE4.zip",
+            checksum: "5a531295977042897bfe94d3e364c92348eecc92e4bdeeeb8b914207999a6988"
         )
     ]
 )
